e8d627163d6754333c01f9de201329ba95811b1e
Zhong-Hua Sun <zhsun@std.uestc.edu.cn>
2025-04-21 23:15:23 +0800

    Update rpcapplication.cc

--- rpcserver/src/rpc/rpcapplication.cc
+++ rpcserver/src/rpc/rpcapplication.cc
@@ -11,39 +11,39 @@ void ShowArgsHelp()
     std::cerr << "format: command -i <configfile>" << std::endl;
 }
 
-void RpcApplication::Init()
+void RpcApplication::Init(int argc, char **argv)
 {
-    // // 判断参数个数是否小于2，小于2则显示参数帮助并退出程序
-    // if (argc < 2)
-    // {
-    //     ShowArgsHelp();
-    //     exit(EXIT_FAILURE);
-    // }
+    // 判断参数个数是否小于2，小于2则显示参数帮助并退出程序
+    if (argc < 2)
+    {
+        ShowArgsHelp();
+        exit(EXIT_FAILURE);
+    }
 
-    // int option = 0;
-    // std::string config_file;
-    // // 使用getopt函数解析参数
-    // while ((option = getopt(argc, argv, "i:")) != -1)
-    // {
-    //     switch (option)
-    //     {
-    //     case 'i':
-    //         // 如果参数为-i，则将optarg赋值给config_file
-    //         config_file = optarg;
-    //         break;
-    //     case '?':
-    //         // 如果参数错误，则显示参数帮助并退出程序
-    //         ShowArgsHelp();
-    //         exit(EXIT_FAILURE);
-    //     case ':':
-    //         // 如果参数缺少值，则显示参数帮助并退出程序
-    //         ShowArgsHelp();
-    //         exit(EXIT_FAILURE);
-    //     default:
-    //         break;
-    //     }
-    // }
-    std::string config_file = "/home/szh/code/TheRPC/bin/test.conf";
+    int option = 0;
+    std::string config_file;
+    // 使用getopt函数解析参数
+    while ((option = getopt(argc, argv, "i:")) != -1)
+    {
+        switch (option)
+        {
+        case 'i':
+            // 如果参数为-i，则将optarg赋值给config_file
+            config_file = optarg;
+            break;
+        case '?':
+            // 如果参数错误，则显示参数帮助并退出程序
+            ShowArgsHelp();
+            exit(EXIT_FAILURE);
+        case ':':
+            // 如果参数缺少值，则显示参数帮助并退出程序
+            ShowArgsHelp();
+            exit(EXIT_FAILURE);
+        default:
+            break;
+        }
+    }
+    //std::string config_file = "/home/szh/code/TheRPC/bin/test.conf";
     config_.LoadConfigFile(config_file);
 }
 
